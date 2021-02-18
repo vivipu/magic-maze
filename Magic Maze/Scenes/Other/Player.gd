cbb0b29128be430062cc7dec635af43337da9855
veevyo <41368076+veevyo@users.noreply.github.com>
2021-02-17 21:04:35 -0600

    Implemented bear sprite, fixed Player.tscn & Enemy.tscn

--- Magic Maze/Scenes/Other/Player.gd
+++ Magic Maze/Scenes/Other/Player.gd
@@ -9,12 +9,18 @@ func _physics_process(delta):
 	var motion = Vector2()
 	if Input.is_action_pressed("up"):
 		motion.y -= 0.75
+		$Sprite.play("Bear")
 	if Input.is_action_pressed("down"):
 		motion.y += 0.75
+		$Sprite.play("Bear")
 	if Input.is_action_pressed("right"):
 		motion.x += 0.75
+		$Sprite.play("Bear")
 	if Input.is_action_pressed("left"):
 		motion.x -= 0.75
+		$Sprite.play("Bear")
+	if Input.is_action_just_released("down") or Input.is_action_just_released("left") or Input.is_action_just_released("right") or Input.is_action_just_released("up"):
+		$Sprite.stop()
 	motion = motion.normalized()
 	motion = move_and_slide(motion * movespeed)
 onready var counter = 5
